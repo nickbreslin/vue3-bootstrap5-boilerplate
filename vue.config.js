6877fb6c1f2aecbc2b2a663166c268597af25fa1
Nick Breslin <nickbreslin@gmail.com>
2021-10-17 00:27:05 -0700

    Capitalize based on package name

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/CHANGE_ME/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "" : "/",
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title =
+        args[0].title.charAt(0).toUpperCase() + args[0].title.slice(1);
+      return args;
+    });
+  },
 };
